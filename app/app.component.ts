479262306c075ce5c4ea77bb9c623c45727441a1
Boris Vasilenko <vasilenko@dreaktor.com>
2016-06-23 21:59:51 +0300

    reproduced issue 9531

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,25 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+     
+      <button (click)="show()" *ngIf="!visible">show</button>
+      <button (click)="hide()" *ngIf="visible">hide</button>
+      
+      <div *ngIf="visible">calculatedBinding: {{calculatedBinding}}</div>
+      `
 })
-export class AppComponent { }
+export class AppComponent {
+  visible = false;
+  get calculatedBinding() {
+    // Issue
+    throw new Error("I will break the page")
+  }
+  show() {
+    this.visible = true;
+    throw new Error("I won't break the page")
+  }
+  hide() {
+    this.visible = false;
+  }
+ }
